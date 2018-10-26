cb944d108df95f077269f85e60adc3c414fc7dea
luyang93 <510426762@qq.com>
2018-10-26 23:03:24 +0800

    Add INI1.py

---
+++ INI1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File    : INI1.py
+# @Date    : 18-10-26
+# @Author  : luyang(luyang@novogene.com)
+
+def main():
+    import this
+
+
+if __name__ == "__main__":
+    main()
