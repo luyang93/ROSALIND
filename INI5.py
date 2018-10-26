cc99ace1f7aaf97ce40310902530fd21975d52ea
luyang93 <510426762@qq.com>
2018-10-26 23:04:09 +0800

    Add INI5.py

---
+++ INI5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File    : INI5.py
+# @Date    : 18-10-26
+# @Author  : luyang(luyang@novogene.com)
+
+def main():
+    file = 'input/rosalind_ini5.txt'
+    with open(file) as f:
+        i = 1
+        for line in f.readlines():
+            if not i % 2:
+                print(line, end='')
+            i += 1
+
+
+if __name__ == "__main__":
+    main()
